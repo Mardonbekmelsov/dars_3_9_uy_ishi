b153bcaf2a7d2fec04b6bfb17c97e009c303c954
Mardonbekmelsov <153820615+Mardonbekmelsov@users.noreply.github.com>
2024-06-03 21:56:20 +0500

    first commit

---
+++ lib/views/screens/home_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'note_screen.dart';
+import 'todo_screens.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 4 / 4,
+      ),
+      children: [
+        InkWell(
+          onTap: () {
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (context) => const ToDoScreen()));
+          },
+          child: Card(
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 32),
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  "Todos",
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+          ),
+        ),
+        InkWell(
+          onTap: () {
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (context) => const NoteScreen()));
+          },
+          child: Card(
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 32),
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  "Notes",
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
