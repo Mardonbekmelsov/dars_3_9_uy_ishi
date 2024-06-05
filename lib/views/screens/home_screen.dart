a54768fc94fa82a33fff91acf0ba5698e8a2bd08
Mardonbekmelsov <153820615+Mardonbekmelsov@users.noreply.github.com>
2024-06-05 10:14:05 +0500

    aa

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,9 +1,13 @@
+import 'package:dars_3_9_uy_ishi/controllers/course_controller.dart';
+import 'package:dars_3_9_uy_ishi/views/screens/course_screen.dart';
 import 'package:flutter/material.dart';
 import 'note_screen.dart';
 import 'todo_screens.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final CourseController courseController = CourseController();
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +19,8 @@ class HomeScreen extends StatelessWidget {
       children: [
         InkWell(
           onTap: () {
-            Navigator.pushReplacement(
-                context, MaterialPageRoute(builder: (context) => const ToDoScreen()));
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => const ToDoScreen()));
           },
           child: Card(
             child: Container(
@@ -33,8 +37,8 @@ class HomeScreen extends StatelessWidget {
         ),
         InkWell(
           onTap: () {
-            Navigator.pushReplacement(
-                context, MaterialPageRoute(builder: (context) => const NoteScreen()));
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => const NoteScreen()));
           },
           child: Card(
             child: Container(
@@ -48,6 +52,56 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
           ),
+        ),
+        FutureBuilder(
+          future: courseController.course(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.red,
+                ),
+              );
+            }
+            if (!snapshot.hasData) {
+              return const Center(
+                child: Text("Kurs mavjud emas"),
+              );
+            }
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error.toString()),
+              );
+            }
+
+            final course = snapshot.data;
+            return InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => CourseScreen(course: course)));
+              },
+              child: Container(
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(course!.imageUrl),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                padding: const EdgeInsets.symmetric(vertical: 32),
+                width: double.infinity,
+                child: Center(
+                  child: Text(
+                    course.title,
+                    style: const TextStyle(fontSize: 24, color: Colors.white),
+                  ),
+                ),
+              ),
+            );
+          },
         ),
       ],
     );
--- lib/views/screens/main_screen.dart
+++ lib/views/screens/main_screen.dart
@@ -14,7 +14,7 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  List<Widget> pages = [const HomeScreen(), ResultsScreen(), ProfileScreen()];
+  List<Widget> pages = [ HomeScreen(), ResultsScreen(), ProfileScreen()];
   List<Widget> titles = [
     const Text("Home Page"),
     const Text("Statistics"),
